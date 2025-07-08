c6fb7ed6ee7c8ac5ae65c60962b49bbee3e4a43c
Vudddagiri Shashank <vshashank2005@gmail.com>
2025-07-08 15:33:46 +0530

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// config.js
-// Add this file to .gitignore!
-window.APP_CONFIG = {
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzNupiZPE6KrB8BaEQJ6o8ZP8gOH_QpmRvKx8xtz5CilyK-EFAIpI9wnuJoI63DzQ6Ygw/exec'
-};
